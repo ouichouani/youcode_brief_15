a750c9d9734381704bee436428199919097de85f
ouichouani <abdelhakim@gmail.com>
2026-02-24 11:19:22 +0000

    initiate the project

---
+++ app/Models/Colocation.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Colocation extends Model
+{
+    /** @use HasFactory<\Database\Factories\ColocationFactory> */
+    use HasFactory;
+}
